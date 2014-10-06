5559f4d749eb2588fcf6d1127aa63080fac34d43
Christian Kniep <christian@qnib.org>
2014-10-06 16:40:06 +0200

    next iteration

--- eval-hpcg/main.go
+++ eval-hpcg/main.go
@@ -19,36 +19,109 @@ import (
 	"github.com/docopt/docopt-go"
 	"github.com/kylelemons/go-gypsy/yaml"
 	"log"
-	//"reflect"
+	"os"
+	"reflect"
 )
+
+/*
+res_map = {
+        "time.total": ['Benchmark Time Summary', 'Total'],
+        "problem.dim.x": ['Global Problem Dimensions', 'Global nx'],
+        "problem.dim.y": ['Global Problem Dimensions', 'Global ny'],
+        "problem.dim.z": ['Global Problem Dimensions', 'Global nz'],
+        "gflops": ['DDOT Timing Variations', 'HPCG result is VALID with a GFLOP/s rating of'],
+        "local.dim.x": ['Local Domain Dimensions', 'nx'],
+        "local.dim.y": ['Local Domain Dimensions', 'ny'],
+        "local.dim.z": ['Local Domain Dimensions', 'nz'],
+        "mach.num_proc": ['Machine Summary', 'Distributed Processes'],
+        "mach.threads_per_proc": ['Machine Summary', 'Threads per processes'],
+    }
+*/
+
+func nodeToMap(node yaml.Node) yaml.Map {
+	m, ok := node.(yaml.Map)
+	if !ok {
+		panic(fmt.Sprintf("%v is not of type map", node))
+	}
+	return m
+}
+
+func nodeToList(node yaml.Node) yaml.List {
+	m, ok := node.(yaml.List)
+	if !ok {
+		panic(fmt.Sprintf("%v is not of type list", node))
+	}
+	return m
+}
+
+func readLastLine(fname string) {
+	file, err := os.Open(fname)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	buf := make([]byte, 32)
+	n, err := file.ReadAt(buf, fi.Size()-int64(len(buf)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	buf = buf[:n]
+	fmt.Printf("%s", buf)
+
+}
 
 func main() {
 	usage := `evaluate HPCG output
 
 Usage:
-  eval-hpcg [options] <params>
+  eval-hpcg [options] <file>
   eval-hpcg -h | --help
   eval-hpcg --version
 
 Options:
-  -y <path>	   	"(Simple) YAML file to read"
   -h --help     Show this screen.
   --version     Show version.
 `
 
 	arguments, _ := docopt.Parse(usage, nil, true, "Naval Fate 2.0", false)
 	fmt.Println(arguments)
-	//file := arguments["-y"]
-	file := "config.yaml"
-	//param := arguments["<param>"]
-	param := "key"
+	file := arguments["<file>"].(string)
+	file_descr, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer file_descr.Close()
+
+	fi, err := file_descr.Stat()
+	if err != nil {
+		fmt.Println(err)
+	}
+	buf := make([]byte, fi.Size())
+	n, err := file_descr.ReadAt(buf, fi.Size()-int64(len(buf)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	buf = buf[:n]
+	fmt.Printf("%s", buf)
 	config, err := yaml.ReadFile(file)
 	if err != nil {
 		log.Fatalf("readfile(%q): %s", file, err)
 	}
-	val, err := config.Get(param)
+	param := "Benchmark Time Summary"
+	subparam := "Total"
+	val := nodeToMap(config.Root)[param]
+	subval := nodeToMap(val)[subparam]
+	fmt.Println(reflect.TypeOf(val))
 	if err != nil {
 		log.Fatalf("read_param(%s): %s", param, err)
 	}
+
 	fmt.Printf("%s = %s\n", param, val)
+	fmt.Printf("%s = %s\n", subparam, subval)
 }
