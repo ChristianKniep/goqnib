6b527001db02d237bb6fb2c1d47bfed653f85d04
Christian Kniep <christian@qnib.org>
2014-10-07 13:21:20 +0200

    running version

--- eval-hpcg/main.go
+++ eval-hpcg/main.go
@@ -15,28 +15,94 @@
 package main
 
 import (
-    "bytes"
+	"./cfg"
+	"bytes"
 	"fmt"
 	"github.com/docopt/docopt-go"
 	"github.com/kylelemons/go-gypsy/yaml"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 /*
 res_map = {
-        "time.total": ['Benchmark Time Summary', 'Total'],
-        "problem.dim.x": ['Global Problem Dimensions', 'Global nx'],
-        "problem.dim.y": ['Global Problem Dimensions', 'Global ny'],
-        "problem.dim.z": ['Global Problem Dimensions', 'Global nz'],
-        "gflops": ['DDOT Timing Variations', 'HPCG result is VALID with a GFLOP/s rating of'],
-        "local.dim.x": ['Local Domain Dimensions', 'nx'],
-        "local.dim.y": ['Local Domain Dimensions', 'ny'],
-        "local.dim.z": ['Local Domain Dimensions', 'nz'],
-        "mach.num_proc": ['Machine Summary', 'Distributed Processes'],
         "mach.threads_per_proc": ['Machine Summary', 'Threads per processes'],
     }
 */
+
+func getFileInfo(file_path string) os.FileInfo {
+	file_descr, err := os.Open(file_path)
+	if err != nil {
+		panic(err)
+	}
+	defer file_descr.Close()
+
+	fi, err := file_descr.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return fi
+}
+
+func parseJobCfg(file_path string) map[string]string {
+	mymap := make(map[string]string)
+	err := cfg.Load(file_path, mymap)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fi := getFileInfo(file_path)
+	const layout = "2006-01-02_15:04:05"
+	mymap["mod_time"] = fi.ModTime().Format(layout)
+	return mymap
+}
+
+func evalDir(path string) (yaml.Node, map[string]string) {
+	files, _ := ioutil.ReadDir(path)
+	job_cfg := make(map[string]string)
+	job_res := *new(yaml.Node)
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".yaml") {
+			job_res = parseHPCG(fmt.Sprintf("%s/%s", path, f.Name()))
+		}
+		if strings.HasSuffix(f.Name(), ".cfg") {
+			job_cfg = parseJobCfg(fmt.Sprintf("%s/%s", path, f.Name()))
+		}
+	}
+	// Fetch datestring
+	re := regexp.MustCompile("20[0-9_-]+")
+	pdate := re.FindStringSubmatch(path)
+	// TODO: []string -> string?
+	if pdate != nil {
+		job_cfg["path_date"] = strings.Join(pdate, "")
+	}
+	return job_res, job_cfg
+
+}
+
+func parseHPCG(file_path string) yaml.Node {
+	file_descr, err := os.Open(file_path)
+	if err != nil {
+		panic(err)
+	}
+	defer file_descr.Close()
+
+	fi, err := file_descr.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	buf := make([]byte, fi.Size())
+	n, err := file_descr.ReadAt(buf, 19)
+	buf = buf[:n]
+	reader := bytes.NewReader(buf)
+	config, err := yaml.Parse(reader)
+	return config
+}
 
 func nodeToMap(node yaml.Node) yaml.Map {
 	m, ok := node.(yaml.Map)
@@ -54,34 +120,20 @@ func nodeToList(node yaml.Node) yaml.List {
 	return m
 }
 
-function ParseHPCG(string file) (yaml.Node ya) {
-	file_descr, err := os.Open(file)
-	if err != nil {
-		panic(err)
+func fetch2D(node yaml.Node, param string) yaml.Node {
+	s := strings.Split(param, "##")
+	val := nodeToMap(node)[strings.Trim(s[0], " ")]
+	if len(s) > 1 {
+		val = nodeToMap(val)[strings.Trim(s[1], " ")]
 	}
-	defer file_descr.Close()
-
-	fi, err := file_descr.Stat()
-	if err != nil {
-		fmt.Println(err)
-	}
-	buf := make([]byte, fi.Size())
-	n, err := file_descr.ReadAt(buf, 19)
-	buf = buf[:n]
-    reader := bytes.NewReader(buf)
-	config, err := yaml.Parse(reader)
-	if err != nil {
-		log.Fatalf("readfile(%q): %s", file, err)
-	}
-    return config
+	return val
 }
-
 
 func main() {
 	usage := `evaluate HPCG output
 
 Usage:
-  eval-hpcg [options] <file>
+  eval-hpcg [options] <path>
   eval-hpcg -h | --help
   eval-hpcg --version
 
@@ -89,18 +141,44 @@ Options:
   -h --help     Show this screen.
   --version     Show version.
 `
-
+	var pwidth map[string]int
+	pwidth = make(map[string]int)
+	var params map[string]string
+	params = make(map[string]string)
+	params["CTIME"] = "Benchmark Time Summary ## Total"
+	pwidth["CTIME"] = 7
+	params["GFLOPs"] = "GFLOP/s Summary ## Raw Total"
+	pwidth["GFLOPs"] = 7
+	params["#PROC"] = "Machine Summary##Distributed Processes"
+	pwidth["#PROC"] = 2
+	/*
+		params["PNx"] = "Processor Dimensions##npx"
+		pwidth["PNx"] = 3
+		params["PNy"] = "Processor Dimensions##npy"
+		pwidth["PNy"] = 3
+		params["PNz"] = "Processor Dimensions##npz"
+		pwidth["PNz"] = 3
+	*/
 	arguments, _ := docopt.Parse(usage, nil, true, "0.1", false)
-	file := arguments["<file>"].(string)
-    ya := ParseHPCG(file)
-	param := "Benchmark Time Summary"
-	subparam := "Total"
-	val := nodeToMap(config)[param]
-	subval := nodeToMap(val)[subparam]
-	if err != nil {
-		log.Fatalf("read_param(%s): %s", param, err)
+	path := arguments["<path>"].(string)
+	job_res, job_cfg := evalDir(path)
+	for key, value := range params {
+		job_cfg[key] = fmt.Sprint(fetch2D(job_res, value))
 	}
-
-	fmt.Printf("%s = %s\n", param, val)
-	fmt.Printf("%s = %s\n", subparam, subval)
+	gflops, _ := strconv.ParseFloat(job_cfg["GFLOPs"], 64)
+	ctime, _ := strconv.ParseFloat(job_cfg["CTIME"], 64)
+	wtime, _ := strconv.ParseFloat(job_cfg["wall_clock"], 64)
+	_, present := job_cfg["path_date"]
+	if present {
+		fmt.Printf("| PTIME:%s |", job_cfg["path_date"])
+	} else {
+		fmt.Printf("| MTIME:%s |", job_cfg["mod_time"])
+	}
+	fmt.Printf(" #PROC:%2s |", job_cfg["#PROC"])
+	fmt.Printf(" NODES:%-27s |", job_cfg["slurm_nodelist"])
+	fmt.Printf(" MPI:%-10s |", job_cfg["mpi_ver"])
+	fmt.Printf(" WTIME:%6.1f |", wtime)
+	fmt.Printf(" CTIME:%6.1f |", ctime)
+	fmt.Printf(" GFLOPS:%8.5f |", gflops)
+	fmt.Printf("\n")
 }
