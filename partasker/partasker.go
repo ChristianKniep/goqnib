198f1c0168b4129328f0121eaa56b00c1f67e31a
Christian Kniep <christian@qnib.org>
2014-11-04 19:47:40 +0800

    parallel tasker according to John Graham-Cumming (presented at dotGo2014

---
+++ partasker/partasker.go
@@ -0,0 +1,133 @@
+// Copyright 2013 Google, Inc.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/***************
+ * Slidly modified version of John Graham-Cumming script
+ * presented at dotGo 2014 (Paris)
+ * source: https://github.com/jgrahamc/dotgo
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	//"net"
+	"os"
+	//"strings"
+	"sync"
+	//"github.com/docopt/docopt-go"
+)
+
+type factory interface {
+	make(line string) task
+}
+
+type task interface {
+	process()
+	print()
+}
+
+type lookup struct {
+	name       string
+	err        error
+	cloudflare bool
+}
+
+func (l *lookup) print() {
+	fmt.Println(l.name)
+}
+func (l *lookup) process() {
+	fmt.Println("Inside process: %s", l.name)
+	/*
+		nss, err := net.LookupNS(l.name)
+		if err != nil {
+			l.err = err
+		} else {
+			for _, ns := range nss {
+				if strings.HasSuffix(ns.Host, ".ns.cludflare.com.") {
+					l.cloudflare = true
+					break
+				}
+			}
+		}
+	*/
+}
+
+type lookupFactory struct {
+}
+
+func (f *lookupFactory) make(line string) task {
+	return &lookup{name: line}
+}
+
+func run(f factory) {
+	var wg sync.WaitGroup
+
+	in := make(chan task)
+
+	wg.Add(1)
+	go func() {
+		s := bufio.NewScanner(os.Stdin)
+		for s.Scan() {
+			in <- f.make(s.Text())
+		}
+		if s.Err() != nil {
+			log.Fatalf("Error reading STDIN: %s", s.Err())
+		}
+		close(in)
+		wg.Done()
+	}()
+
+	out := make(chan task)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for t := range in {
+				t.process()
+				out <- t
+			}
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	for t := range out {
+		t.print()
+	}
+}
+
+func main() {
+	/*
+			usage := `evaluate HPCG output
+
+		Usage:
+		  eval-hpcg [options] <path>
+		  eval-hpcg -h | --help
+		  eval-hpcg --version
+
+		Options:
+		  -h --help     Show this screen.
+		  --version     Show version.
+		`
+			arguments, _ := docopt.Parse(usage, nil, true, "0.1", false)
+			path := arguments["<path>"].(string)
+	*/
+	run(&lookupFactory{})
+}
